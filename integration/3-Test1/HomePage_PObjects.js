67ab7df9ee4fca11ef5a5e8b6374034dd5b76d34
Franco <69213724+francoec@users.noreply.github.com>
2023-04-12 10:24:45 -0300

    new test in progress

---
+++ integration/3-Test1/HomePage_PObjects.js
@@ -0,0 +1,33 @@
+import indexPage from '../pages/index'
+
+describe('Home de la page', () =>{
+    beforeEach(() => {
+        cy.visit('https://www.movistar.com.ar/')
+    })
+
+    it('CP01 should filter for motorola', () => {
+        cy.title().should('include', 'Internet Fibra, Planes Móviles y Celulares - Movistar Argentina')
+        cy.contains('Mi Movistar')
+        indexPage.FiltrarMotorola()
+        cy.wait(5000)
+        /*cy.get('.device-carousel-title > a').click()
+        cy.get('#layered-filter-block > .block-title > strong').click()
+        cy.get('[attribute="manufacturer"] > .filter-options-content > .items > :nth-child(1) > a').click()*/
+    })
+
+    it('CP02 should filter for Samsung', () => {
+        indexPage.FiltrarSamsung()
+        cy.wait(5000)
+    })
+
+    it('CP03 should filter for TCL', () => {
+        indexPage.FiltrarTCL()
+        cy.wait(5000)
+    })
+
+    it('CP04 should filter for Xiaomi', () => {
+        indexPage.FiltrarXiaomi()
+        cy.wait(5000)
+    })
+
+})
---
+++ integration/pages/index.js
@@ -0,0 +1,37 @@
+ class indexPage{
+    constructor(){
+      this.VerMas = '.device-carousel-title > a';
+      this.FiltrarPor = '#layered-filter-block > .block-title > strong';
+      this.Motorola = '[attribute="manufacturer"] > .filter-options-content > .items > :nth-child(1) > a';
+      this.Samsung = '[attribute="manufacturer"] > .filter-options-content > .items > :nth-child(2) > a';
+      this.TCL = '[attribute="manufacturer"] > .filter-options-content > .items > :nth-child(3) > a';
+      this.Xiaomi = '[attribute="manufacturer"] > .filter-options-content > .items > :nth-child(4) > a';
+    }
+
+    FiltrarMotorola = () =>{
+        cy.get(this.VerMas).click();
+        cy.get(this.FiltrarPor).click();
+        cy.get(this.Motorola).click();
+    }
+
+    FiltrarSamsung = () =>{
+      cy.get(this.VerMas).click();
+      cy.get(this.FiltrarPor).click();
+      cy.get(this.Samsung).click();
+    }
+
+    FiltrarTCL = () =>{
+      cy.get(this.VerMas).click();
+      cy.get(this.FiltrarPor).click();
+      cy.get(this.TCL).click();
+    }
+
+    FiltrarXiaomi = () =>{
+      cy.get(this.VerMas).click();
+      cy.get(this.FiltrarPor).click();
+      cy.get(this.Xiaomi).click();
+    }
+
+
+ }
+ export default new indexPage
